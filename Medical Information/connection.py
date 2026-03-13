cac5a744eacc88b334d0d907c2647a43d553425b
ksenijasam <ksenija2702@gmail.com>
2026-03-13 18:24:49 +0100

    .gitignore + connection py-snowflake

---
+++ Medical Information/connection.py
@@ -0,0 +1,15 @@
+import snowflake.connector
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+conn = snowflake.connector.connect(
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    account=os.getenv("account"),
+    warehouse=os.getenv("warehouse"),
+    database=os.getenv("database"),
+    schema=os.getenv("schema")
+)
